c4b3677638f71fba31bead8983c497ae66ce163d
VByteHav <vaibhavcodex11@gmail.com>
2025-03-19 14:53:36 +0530

    Create MTC

---
+++ music-time-capsule/app/dashboard/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Dashboard from '@/components/Dash'
+import Navbar from '@/components/Navbar'
+const page = () => {
+  return (
+    <div>
+            <Navbar/>
+      <Dashboard/>
+    </div>
+  )
+}
+
+export default page
+export const metadata = {
+  title: "Dashboard - Music Time Capsule",
+  description: "Dashboard for your music journey. Here you can see your Music Stats",
+
+}
